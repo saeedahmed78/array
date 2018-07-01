a48545d1d058afea14c31be06ff9990828abf43e
saeedahmed78 <38943150+saeedahmed78@users.noreply.github.com>
2018-07-01 05:49:36 -0700

    array

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let arayyy = []
+function number(){
+	var num = parseInt(document.querySelector(".input1").value);
+	arayyy.push(num);
+	var div = document.querySelector(".alert-shownumber");
+		div.innerHTML = arayyy;
+
+
+}
+function sum(){
+	var div = document.querySelector(".alert_result");
+		sum =  eval(arayyy.join('+')); 
+	div.innerHTML = sum;
+	
+}
+function avg(){
+	var div = document.querySelector(".alert_result");
+		avg = eval(arayyy.join('+')+('/arayyy.length'));
+	div.innerHTML = avg;
+	
+}
+function product(){
+	var div = document.querySelector(".alert_result");
+		sum =  eval(arayyy.join('*')); 
+	div.innerHTML = sum;
+	
+}
